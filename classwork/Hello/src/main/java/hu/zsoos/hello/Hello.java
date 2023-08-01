6928fc6a81b819e40c8b3d9ec8a4a8378ec6b900
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-01 12:20:38 +0200

    Hello Name print to console

---
+++ classwork/Hello/src/main/java/hu/zsoos/hello/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hu.zsoos.hello;
+
+/**
+ *
+ * @author Z-SooS
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Zoltán!");
+    }
+}
