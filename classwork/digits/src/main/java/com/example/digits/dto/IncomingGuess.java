992cca164567f1742bfe041311ebe31c57c0ce22
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-29 18:29:11 +0530

    Change validation to constant variable

--- classwork/digits/src/main/java/com/example/digits/dto/IncomingGuess.java
+++ classwork/digits/src/main/java/com/example/digits/dto/IncomingGuess.java
@@ -1,12 +1,12 @@
 package com.example.digits.dto;
 
-import com.example.digits.model.Round;
+import com.example.digits.service.DigitService;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class IncomingGuess {
     private int gameId;
-    @Size(max = 4)
+    @Size(max = DigitService.NUMBER_OF_DIGITS)
     private String guess;
 }
